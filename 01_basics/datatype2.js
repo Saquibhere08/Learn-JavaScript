adfdf25326ffd64e9282fd9084cbc41548fe49f9
Saquib Bin Halim <halimsaquib99@gmail.com>
2026-08-19 12:33:27 +0000

    Basics

---
+++ 01_basics/datatype2.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer versions.
+
+let name="Saquib"
+let age=24
+let isLoggedIn=false
+
+//DataTypes:
+//number ===> 2 to power 53
+// bigInt
+// string ===> " "
+// boolean ===> true or false
+// null ===> standalone value
+// undefined ===>
+// symbol ===> unique
+
+// object
+
+console.log(typeof "Saquib");
+console.log(typeof null);   //object
+console.log(typeof undefined);  //undefined
+
+
