b99ba22c5c84f173b9575bba99e69d49935e2605
Saquib Bin Halim <halimsaquib99@gmail.com>
2026-08-19 12:41:08 +0000

    Conversions in JavaScripts

--- 01_basics/conversion.js
+++ 01_basics/conversion.js
@@ -1,5 +1,7 @@
 let score =33
 score = "33abc"
+score=null
+score=true
 console.log(typeof(score));
 console.log(typeof score);
 
@@ -7,6 +9,20 @@ let valueInNumber =Number(score)
 console.log(typeof valueInNumber);
 console.log(valueInNumber); //NaN - Not a Number
 
+let isLoggedIn=1
+isLoggedIn=0
+isLoggedIn=""
+isLoggedIn="Saquib"
 
+let booleanIsloggedIn = Boolean(isLoggedIn)
+console.log(typeof(booleanIsloggedIn));
+console.log(booleanIsloggedIn);
+
+
+
+
+//"33" ==> 33
+//"33abc" ==> NaN
+// true ==> 1 & false ==> 0
 
 
