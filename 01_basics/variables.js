70493cc8560abb5ba00c30ffc640a7b814f81319
Saquib Bin Halim <halimsaquib99@gmail.com>
2026-08-19 12:12:03 +0000

    updated tree

---
+++ 01_basics/variables.js
@@ -0,0 +1,13 @@
+const accountId=1445566
+let accountEmail="saquib@gmail.com"
+var accountPass="12345"
+accountCity="Jaipur"
+let accountState
+// accountId=2 - not allowed when defined as const
+console.log(accountId);
+
+accountEmail="hc@gmail.com"
+accountPass="34212"
+accountCity="Hyderabad"
+
+console.table([accountEmail,accountPass,accountCity,accountState])
