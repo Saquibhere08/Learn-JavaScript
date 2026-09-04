69633044bc6d8874b444320872a4167e026027eb
Saquib Bin Halim <halimsaquib99@gmail.com>
2026-09-04 14:09:21 +0530

    Basic Operations

---
+++ 04_operations/operations1.js
@@ -0,0 +1,19 @@
+//Example1
+let value=3
+let negValue=-value
+console.log(negValue);
+
+//Example2
+let str1="Saquib"
+let str2=" hello"
+let str3=str1+str2
+console.log(str3);
+
+//Example3
+//"First string encountered → subsequent + operations become concatenation."
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+
