ba12fa5d03e71d35254d63abce702de374ad315e
Saquib Bin Halim <halimsaquib99@gmail.com>
2026-09-06 16:47:13 +0530

    Strings

---
+++ 06_Strings/string1.js
@@ -0,0 +1,35 @@
+//Example1
+const name="saquib"
+const repoC=69
+
+console.log(name+repoC+ " value: 100");
+
+//Example2
+console.log(`hello my name is ${name} and my repo count is ${repoC}`);
+
+//Example3
+const gameName=new String('bigPapa')
+console.log(gameName);
+console.log(gameName[0]);
+
+//Important Methods/Functions
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('p'));
+
+//Example4
+const newString=gameName.substring(2,5)
+console.log(newString);
+
+const anotherName=gameName.slice(-9,4)
+console.log(anotherName);
+console.log(gameName.split('-'));
+
+
+
+
+
+
+
---
+++ 06_Strings/string2.js
@@ -0,0 +1,14 @@
+//Example5
+
+const newStr="   Saquib   "
+console.log(newStr);
+console.log(newStr.trim());
+
+//Example6
+const url="https://saquib.com/saquib%20halim@gmail.com"
+console.log(url);
+console.log(url.replace('%20',"-"));
+console.log(url.includes('saquib'));
+console.log(url.includes('andd'));
+
+
